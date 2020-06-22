0e9ad631b5f61bee5d3ae19e461838a5bcb656fd
Kelly online <kellyonline2016@gmail.com>
2020-06-22 10:01:48 +0100

    create and listen to server on port, CONFIG MONGO

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+
+
+
+// To load the config file configuration
+
+dotenv.config({ path: './config/conf.env'})
+
+const app = express();
+
+app.listen()
